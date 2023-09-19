150b15ff7ff0f72a4a221c81db8ae2e926fcd3dd
ana vitória <129559801+avglg@users.noreply.github.com>
2023-09-19 20:30:05 -0300

    Add files via upload

---
+++ Projeto.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ALUNO PROZ
+ */
+public class Projeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+       Scanner in = new Scanner(System.in);
+       String nome;
+       int idade ;
+       System.out.println("digite qual seu nome:  ");
+       nome = in.nextLine();
+       System.out.println("digite qual sua idade:  ");
+       idade = in.nextInt();
+       System.out.println("Seja bem vindo " + nome + ", sua idade é:" + idade);
+    
+       
+               
+      
+      
+        
+    }
+    
+}
