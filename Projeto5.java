af40fc909a6f082b13dd8b7cc5001704bb330b1a
ana vitória <129559801+avglg@users.noreply.github.com>
2023-09-20 20:48:21 -0300

    Add files via upload
    crie algoritimo que leia o valor do salario minimo e o valor do salario e calcule a quantidade de salarios minimos que esse usuario ganha

---
+++ Projeto5.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ALUNO PROZ
+ */
+public class Projeto33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          Scanner in = new Scanner(System.in);
+        double qtdsalariomin;
+        double salario;
+        double salariomin;
+        System.out.println("informe o salario minimo e seu salario?:  ");
+           salariomin = in.nextInt();
+           salario = in.nextInt();
+        qtdsalariomin = salario / salariomin;
+        System.out.println("quantidades de salarios minimos que voce ganha:  " + qtdsalariomin );
+
+    }
+}
+
+            
+            
+         
+       
